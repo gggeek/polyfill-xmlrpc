8457b9c266400369806c2585891bb4ddce63eb24
gggeek <giunta.gaetano@gmail.com>
2020-12-25 16:56:57 +0000

    WIP

--- README.md
+++ README.md
@@ -2,6 +2,9 @@ A pure-php reimplementation of the API exposed by the native XML-RPC extension.
 
 Originally bundled as part of the [phpxmlrpc/extras](https://github.com/gggeek/phpxmlrpc-extras) package.
 
+Known differences from the original extension
+---------------------------------------------
+
 License
 -------
 Use of this software is subject to the terms in the [license.txt](license.txt) file
--- src/XmlRpc.php
+++ src/XmlRpc.php
@@ -19,6 +19,7 @@
  * - a single NULL value passed to xmlrpc_encode_req(null, $val) will be decoded as '', not NULL
  *   (the extension generates an invalid xmlrpc response in this case)
  * - the native extension truncates double values to 6 decimal digits, we do not
+ * -  xmlrpc_server_create returns an object instead of a resource
  *
  * @todo finish implementation of 3 missing functions
  */
@@ -27,6 +28,8 @@ namespace PhpXmlRpc\Polyfill\XmlRpc;
 
 use PhpXmlrpc\Encoder;
 use PhpXmlrpc\Request;
+use PhpXmlrpc\Response;
+use PhpXmlrpc\Server;
 use PhpXmlrpc\Value;
 
 final class XmlRpc
@@ -38,15 +41,17 @@ final class XmlRpc
      * @return mixed
      *
      * @todo implement usage of $encoding
+     * @todo test against upstream: is default encoding really latin-1 ?
      */
-    public static function xmlrpc_decode($xml, $encoding = '')
+    public static function xmlrpc_decode($xml, $encoding = "iso-8859-1")
     {
+        $encoder = new PhpXmlRpc\Encoder();
         // strip out unnecessary xml in case we're deserializing a single param.
         // in case of a complete response, we do not have to strip anything
         // please note that the test below has LARGE space for improvement (eg it might trip on xml comments...)
         if (strpos($xml, '<methodResponse>') === false)
             $xml = preg_replace(array('!\s*<params>\s*<param>\s*!', '!\s*</param>\s*</params>\s*$!'), array('', ''), $xml);
-        $val = php_xmlrpc_decode_xml($xml);
+        $val = $encoder->decodeXml($xml);
         if (!$val) {
             return null; // instead of false
         }
@@ -54,10 +59,10 @@ final class XmlRpc
             if ($fc = $val->faultCode()) {
                 return array('faultCode' => $fc, 'faultString' => $val->faultString());
             } else {
-                return php_xmlrpc_decode($val->value(), array('extension_api'));
+                return $encoder->decode($val->value(), array('extension_api'));
             }
         } else
-            return php_xmlrpc_decode($val, array('extension_api'));
+            return $encoder->decode($val, array('extension_api'));
     }
 
     /**
@@ -68,10 +73,12 @@ final class XmlRpc
      * @return mixed
      *
      * @todo implement usage of $encoding
+     * @todo test against upstream: is default encoding really null ?
      */
-    public static function xmlrpc_decode_request($xml, &$method, $encoding = '')
+    public static function xmlrpc_decode_request($xml, &$method, $encoding = null)
     {
-        $val = php_xmlrpc_decode_xml($xml);
+        $encoder = new PhpXmlRpc\Encoder();
+        $val = $encoder->decodeXml($xml);
         if (!$val) {
             return null; // instead of false
         }
@@ -79,14 +86,14 @@ final class XmlRpc
             if ($fc = $val->faultCode()) {
                 $out = array('faultCode' => $fc, 'faultString' => $val->faultString());
             } else {
-                $out = php_xmlrpc_decode($val->value(), array('extension_api'));
+                $out = $encoder->decode($val->value(), array('extension_api'));
             }
         } else if (is_a($val, 'xmlrpcmsg')) {
             $method = $val->method();
             $out = array();
             $pn = $val->getNumParams();
             for ($i = 0; $i < $pn; $i++)
-                $out[] = php_xmlrpc_decode($val->getParam($i), array('extension_api'));
+                $out[] = $encoder->decode($val->getParam($i), array('extension_api'));
         } else
             return null; /// @todo test lib behaviour in this case
 
@@ -100,60 +107,63 @@ final class XmlRpc
      */
     public static function xmlrpc_encode($val)
     {
-        $val = php_xmlrpc_encode($val, array('extension_api'));
+        $encoder = new PhpXmlRpc\Encoder();
+        $val = $encoder->encode($val, array('extension_api'));
         return "<?xml version=\"1.0\" ?" . ">\n<params>\n<param>\n" . $val->serialize() . "</param>\n</params>";
     }
 
     /**
      * Given a method name and array of php values, create an xmlrpc request out
      * of them. If method name === null, will create an xmlrpc response
-     * @param string $meth
-     * @param array $vals
-     * @param array $opts options array
+     * @param string $method
+     * @param array $params
+     * @param array $output_options options array
      * @return string
      *
      * @todo implement parsing/usage of options
      */
-    public static function xmlrpc_encode_request($meth, $vals, $opts = null)
+    public static function xmlrpc_encode_request($method, $params, $output_options = array())
     {
-        $opts = array_merge($opts, array('extension_api'));
+        $encoder = new Encoder();
 
-        if ($meth !== null) {
+        $output_options = array_merge($output_options, array('extension_api'));
+
+        if ($method !== null) {
             // mimic EPI behaviour: if ($val === NULL) then send NO parameters
-            if (!is_array($vals)) {
-                if ($vals === NULL) {
-                    $vals = array();
+            if (!is_array($params)) {
+                if ($params === NULL) {
+                    $params = array();
                 } else {
-                    $vals = array($vals);
+                    $params = array($params);
                 }
             } else {
                 // if given a 'hash' array, encode it as a single param
                 $i = 0;
                 $ok = true;
-                foreach ($vals as $key => $value)
+                foreach ($params as $key => $value)
                     if ($key !== $i) {
                         $ok = false;
                         break;
                     } else
                         $i++;
                 if (!$ok) {
-                    $vals = array($vals);
+                    $params = array($params);
                 }
             }
             $values = array();
-            foreach ($vals as $key => $value) {
-                $values[] = php_xmlrpc_encode($value, $opts);
+            foreach ($params as $key => $value) {
+                $values[] = $encoder->encode($value, $output_options);
             }
 
             // create request
-            $req = new xmlrpcmsg($meth, $values);
+            $req = new Request($method, $values);
             $resp = $req->serialize();
         } else {
             // create response
-            if (is_array($vals) && xmlrpc_is_fault($vals))
-                $req = new xmlrpcresp(0, (integer)$vals['faultCode'], (string)$vals['faultString']);
+            if (is_array($params) && xmlrpc_is_fault($params))
+                $req = new Response(0, (integer)$params['faultCode'], (string)$params['faultString']);
             else
-                $req = new xmlrpcresp(php_xmlrpc_encode($vals, $opts));
+                $req = new Response($encoder->encode($params, $output_options));
             $resp = "<?xml version=\"1.0\"?" . ">\n" . $req->serialize();
         }
         return $resp;
@@ -161,12 +171,12 @@ final class XmlRpc
 
     /**
      * Given a php value, return its corresponding xmlrpc type
-     * @param mixed $val
+     * @param mixed $value
      * @return string
      */
-    public static function xmlrpc_get_type($val)
+    public static function xmlrpc_get_type($value)
     {
-        switch (strtolower(gettype($val))) {
+        switch (strtolower(gettype($value))) {
             case 'string':
                 return $GLOBALS['xmlrpcString'];
             case 'integer':
@@ -179,7 +189,7 @@ final class XmlRpc
             case 'array':
                 $i = 0;
                 $ok = true;
-                foreach ($val as $key => $value)
+                foreach ($value as $key => $valueue)
                     if ($key !== $i) {
                         $ok = false;
                         break;
@@ -188,14 +198,117 @@ final class XmlRpc
 
                 return $ok ? $GLOBALS['xmlrpcArray'] : $GLOBALS['xmlrpcStruct'];
             case 'object':
-                if (is_a($val, 'xmlrpcval')) {
-                    list($type, $val) = each($val->me);
+                if (is_a($value, 'xmlrpcval')) {
+/// @todo fixme
+                    list($type, $value) = each($value->me);
                     return str_replace(array('i4', 'dateTime.iso8601'), array('int', 'datetime'), $type);
                 }
                 return $GLOBALS['xmlrpcStruct'];
             case 'null':
                 return $GLOBALS['xmlrpcBase64']; // go figure why...
         }
+    }
+
+    /**
+     * Checks if a given php array corresponds to an xmlrpc fault response
+     * @param array $arg
+     * @return boolean
+     */
+    public static function xmlrpc_is_fault($arg)
+    {
+        return is_array($arg) && array_key_exists('faultCode', $arg) && array_key_exists('faultString', $arg);
+    }
+
+    /**
+     * @param string $xml
+     * @return array
+     * @todo implement
+     */
+    public static function xmlrpc_parse_method_descriptions($xml)
+    {
+        return array();
+    }
+
+    /** Server side ***************************************************************/
+
+    /**
+     * @param Server $server
+     * @param array $desc
+     * @return int
+     * @todo implement
+     */
+    public static function xmlrpc_server_add_introspection_data($server, $desc)
+    {
+        return 0;
+    }
+
+    /**
+     * Parses XML request and calls corresponding method
+     * @param Server $server
+     * @param string $xml
+     * @param mixed $user_data
+     * @param array $output_options
+     * @return string
+     */
+    public static function xmlrpc_server_call_method($server, $xml, $user_data, $output_options = array())
+    {
+        $server->user_data = $user_data;
+        return $server->service($xml, true);
+    }
+
+    /**
+     * Create a new xmlrpc server instance
+     * @return Server
+     */
+    public static function xmlrpc_server_create()
+    {
+        $s = new Server();
+        $s->functions_parameters_type = 'epivals';
+        $s->compress_response = false; // since we will not be outputting any http headers to go with it
+        return $s;
+    }
+
+    /**
+     * This function actually does nothing, but it is kept for compatibility.
+     * To destroy a server object, just unset() it, or send it out of scope...
+     * @param Server $server
+     * @return integer
+     */
+    public static function xmlrpc_server_destroy($server)
+    {
+        if (is_a($server, 'xmlrpc_server'))
+            return 1;
+        else
+            return 0;
+    }
+
+    /**
+     * @param Server $server
+     * @param string $function
+     * @return bool
+     *
+     * @todo implement
+     */
+    public static function xmlrpc_server_register_introspection_callback($server, $function)
+    {
+        return false;
+    }
+
+    /**
+     * Add a php function as xmlrpc method handler to an existing server.
+     * PHP function sig: f(string $methodname, array $params, mixed $extra_data)
+     * @param Server $server
+     * @param string $method_name
+     * @param string $function
+     * @return boolean true on success or false
+     */
+    public static function xmlrpc_server_register_method($server, $method_name, $function)
+    {
+        if (is_a($server, 'xmlrpc_server')) {
+            $server->add_to_map($method_name, $function);
+            return true;
+        } else
+            return false;
     }
 
     /**
@@ -209,13 +322,13 @@ final class XmlRpc
     {
         if (is_string($val)) {
             if ($type == 'base64') {
-                $val = new xmlrpcval($val, 'base64');
+                $val = new Value($val, 'base64');
                 // add two object members to make it more compatible to user code
                 $val->scalar = $val->me['base64'];
                 $val->xmlrpc_type = 'base64';
             } elseif ($type == 'datetime') {
                 if (preg_match('/([0-9]{8})T([0-9]{2}):([0-9]{2}):([0-9]{2})/', $val)) {
-                    $val = new xmlrpcval($val, 'dateTime.iso8601');
+                    $val = new Value($val, 'dateTime.iso8601');
                     // add 3 object members to make it more compatible to user code
                     $val->scalar = $val->me['dateTime.iso8601'];
                     $val->xmlrpc_type = 'datetime';
@@ -232,89 +345,5 @@ final class XmlRpc
         } else {
             return false;
         }
-    }
-
-    /**
-     * Checks if a given php array corresponds to an xmlrpc fault response
-     * @param array $arr
-     * @return boolean
-     */
-    public static function xmlrpc_is_fault($arr)
-    {
-        return is_array($arr) && array_key_exists('faultCode', $arr) && array_key_exists('faultString', $arr);
-    }
-
-    /** Server side ***************************************************************/
-
-    /**
-     * Create a new xmlrpc server instance
-     * @return xmlrpc_server
-     */
-    public static function xmlrpc_server_create()
-    {
-        $s = new xmlrpc_server();
-        $s->functions_parameters_type = 'epivals';
-        $s->compress_response = false; // since we will not be outputting any http headers to go with it
-        return $s;
-    }
-
-    /**
-     * This function acutally does nothing, but it is kept for compatibility.
-     * To destroy a server object, just unset() it, or send it out of scope...
-     * @param xmlrpc_server $server
-     * @return integer
-     */
-    public static function xmlrpc_server_destroy($server)
-    {
-        if (is_a($server, 'xmlrpc_server'))
-            return 1;
-        else
-            return 0;
-    }
-
-    /**
-     * Add a php function as xmlrpc method handler to an existing server.
-     * PHP function sig: f(string $methodname, array $params, mixed $extra_data)
-     * @param xmlrpc_server $server
-     * @param string $method_name
-     * @param string $function
-     * @return boolean true on success or false
-     */
-    public static function xmlrpc_server_register_method($server, $method_name, $function)
-    {
-        if (is_a($server, 'xmlrpc_server')) {
-            $server->add_to_map($method_name, $function);
-            return true;
-        } else
-            return false;
-    }
-
-    /**
-     * Parses XML request and calls corresponding method
-     * @param xmlrpc_server $server
-     * @param string $xml
-     * @param mixed $user_data
-     * @param array $output_options
-     * @return string
-     */
-    public static function xmlrpc_server_call_method($server, $xml, $user_data, $output_options = null)
-    {
-        $server->user_data = $user_data;
-        return $server->service($xml, true);
-    }
-
-    public static function xmlrpc_parse_method_descriptions($xml)
-    {
-        return array();
-    }
-
-    public static function xmlrpc_server_add_introspection_data($server, $desc)
-    {
-        return 0;
-    }
-
-    public static function xmlrpc_server_register_introspection_callback($server, $function)
-    {
-        return false;
     }
 }
